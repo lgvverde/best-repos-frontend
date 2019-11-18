e2e45e334733011709a3d9723a26bc8b31aea8e8
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2019-11-18 16:59:48 -0300

    Aumento do limite de tempo na API

--- src/api/config.js
+++ src/api/config.js
@@ -3,10 +3,10 @@ const axios = require('axios');
 
 export const GITHUB_API = axios.create({
     baseURL: process.env.REACT_APP_GITHUB_API_URL,
-    timeout: 5000
+    timeout: 10000
 });
 
 export const MY_REPO_API = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-    timeout: 5000
+    timeout: 10000
 });
